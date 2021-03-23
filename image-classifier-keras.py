b110116fefbd409b3deaa455206eabe32cb80547
hawrot <mhawro200@caledonian.ac.uk>
2021-03-23 20:26:21 +0100

    Reformat the code

--- image-classifier-keras.py
+++ image-classifier-keras.py
@@ -9,23 +9,17 @@ fashion_mnist = keras.datasets.fashion_mnist
 
 (X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
 
-# Shape check
-#print(X_train_full.shape)
-
-# Type
-#print(X_train_full.dtype)
+#print(X_train_full.shape) # Shape check
+#print(X_train_full.dtype) # Type chek
 
 # Prepare the validation set
 X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0 # Divided by 255 to scale the pixels for Gradient Descent
 y_valid, y_train = y_train_full[:5000] / 255.0, y_train_full[5000:] / 255.0
 
-
 # Set the classes
-
 class_names = ['T-shirt/top', 'Trousers', 'Pullover', 'Dress', 'Coat', 'Sandals', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 # Build the model
-
 model = keras.models.Sequential()
 model.add(keras.layers.Flatten(input_shape=[28,28])) # Convert each input image into 1D array
 model.add(keras.layers.Dense(300, activation="relu")) # 300 neurons | Dense layers manage weight matrix by its own
