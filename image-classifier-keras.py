2070f8b2475b8a7c5eeaa20f5ecb3e6339d42604
hawrot <mhawro200@caledonian.ac.uk>
2021-03-23 20:17:18 +0100

    prepare the label classes

--- image-classifier-keras.py
+++ image-classifier-keras.py
@@ -18,3 +18,9 @@ fashion_mnist = keras.datasets.fashion_mnist
 # Prepare the validation set
 X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0 # Divided by 255 to scale the pixels for Gradient Descent
 y_valid, y_train = y_train_full[:5000] / 255.0, y_train_full[5000:] / 255.0
+
+
+# Set the classes
+
+class_names = ['T-shirt/top', 'Trousers', 'Pullover', 'Dress', 'Coat', 'Sandals', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
