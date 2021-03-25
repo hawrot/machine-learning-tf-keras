b6eb2f9f289773dd051b9f5bf8ede947411929b4
hawrot <mhawro200@caledonian.ac.uk>
2021-03-25 23:39:15 +0100

    Add output

--- CNN.py
+++ CNN.py
@@ -12,9 +12,9 @@ images = np.array([china, flower])
 batch_size, height, width, channels = images.shape
 
 # Create two filters
+filters = np.zeros(shape=(7,7, channels, 2), dtype=np.float32)
+filters[:, 3, :, 0] = 1 # vertical line
+filters[3, :, :, 1] = 1 # horizontal line
 
-filers = np.zeros(shape=(7,7, channels, 2), dtype=np.float32)
-filers2 = tf.zeros(shape=(7,7, channels, 2), dtype=np.float32)
-
-print(filers)
-print(filers2)
+# Outputs
+outputs = tf.nn.conv2d(images, filters, strides=1, padding="same")
