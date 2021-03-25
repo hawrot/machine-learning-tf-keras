ea00f43581bd06a7141c9e0f8eb0fd084b3f5d78
hawrot <mhawro200@caledonian.ac.uk>
2021-03-25 23:36:27 +0100

    Create filters

--- CNN.py
+++ CNN.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 from sklearn.datasets import load_sample_image
 
 # Load sample images
@@ -6,3 +7,14 @@ china = load_sample_image("china.jpg") / 255
 flower = load_sample_image("flower.jpg") / 255
 
 images = np.array([china, flower])
+#print(images.shape) # Check the shape | 2 , 427, 640, 3
+
+batch_size, height, width, channels = images.shape
+
+# Create two filters
+
+filers = np.zeros(shape=(7,7, channels, 2), dtype=np.float32)
+filers2 = tf.zeros(shape=(7,7, channels, 2), dtype=np.float32)
+
+print(filers)
+print(filers2)
