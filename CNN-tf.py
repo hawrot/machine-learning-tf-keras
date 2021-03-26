d3941a2884b0780c867616c00fc20390e49be848
hawrot <mhawro200@caledonian.ac.uk>
2021-03-26 18:00:53 +0100

    normalize the values

--- CNN-tf.py
+++ CNN-tf.py
@@ -6,3 +6,6 @@ import matplotlib.pyplot as plt
 # Download and prepare the CIFAR10 dataset which contains 60k images in 10 classes
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
