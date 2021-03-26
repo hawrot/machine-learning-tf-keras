d0f62a926e864e91de95f8b84f7681b2df697491
hawrot <mhawro200@caledonian.ac.uk>
2021-03-26 18:14:59 +0100

    compile, train, evaluate

--- CNN-tf.py
+++ CNN-tf.py
@@ -39,3 +39,18 @@ model.add(layers.Dense(10))
 
 # Summarise the model
 model.summary()
+
+# Compile and Train
+model.compile(optimizer='adam', loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=30, validation_data=(test_images, test_labels))
+
+# Evaluate model
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
