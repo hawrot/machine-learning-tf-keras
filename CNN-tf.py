8f46370c066cee6e1c2a9dfb913c0fbe75871b80
hawrot <mhawro200@caledonian.ac.uk>
2021-03-26 18:04:34 +0100

    verify the data by plotting images

--- CNN-tf.py
+++ CNN-tf.py
@@ -9,3 +9,15 @@ import matplotlib.pyplot as plt
 # Normalize pixel values to be between 0 and 1
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
+# Verify the data
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlable(class_names[train_labels[i][0]])
+plt.show()
