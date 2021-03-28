de276bd548a283a9bb60ecb121b95297e8b8b194
hawrot <mhawro200@caledonian.ac.uk>
2021-03-28 17:08:56 +0200

    Construct the model

---
+++ CNN-keras.py
@@ -0,0 +1,22 @@
+# Imports
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # 1 input channel
+        # 6 outputs channel
+        # 3 x 3 square convolution
+
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+
+        # y = Wx + b
+        self.fc1 = nn.Linear(16 * 6 * 6, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+
