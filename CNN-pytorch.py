039ec2df42b99fc8111a752aa0e1d421c847c3d9
hawrot <mhawro200@caledonian.ac.uk>
2021-03-28 17:21:28 +0200

    Reformat the code

--- CNN-pytorch.py
+++ CNN-pytorch.py
@@ -1,48 +1,23 @@
 # Imports
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
 
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        # 1 input channel
-        # 6 outputs channel
-        # 3 x 3 square convolution
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-        self.conv1 = nn.Conv2d(1, 6, 3)
-        self.conv2 = nn.Conv2d(6, 16, 3)
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
 
-        # y = Wx + b
-        self.fc1 = nn.Linear(16 * 6 * 6, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
 
-    def forward(self, x):
-        # max pooling (2, 2)
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-
-        # Single number if the size is square
-        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
-
-net = Net()
-print(net)
-
-# Define the learnable params
-params = list(net.parameters())
-print(len(params))
-print(params[0].size())  # conv1's .weight
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
